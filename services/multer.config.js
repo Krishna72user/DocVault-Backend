702697c9154fe255360a5e418de569803ac87364
Krishna <anonymous@google.com>
2025-03-16 12:26:17 +0530

    Minor improvements

--- services/multer.config.js
+++ services/multer.config.js
@@ -9,7 +9,7 @@ if (!fs.existsSync(tmpDir)) {
 }
 const storage = multer.diskStorage({
     destination:function (req,file,cb){
-        cb(null,'./tmp');
+        cb(null,tmpDir);
     },
     filename:function (req,file,cb){
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
